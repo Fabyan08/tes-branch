a09c76a2619d252c28676b85047bbbe730fba22e
Fabyan Yastika Permana <fabyanxrpl@gmail.com>
2023-12-23 15:47:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#272B35",
+        second: "#5FAEC4",
+        grey: "#252B42",
+        grey2: "#737373"
+      },
+      fontFamily: {
+        urbanist: ["Urbanist", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
