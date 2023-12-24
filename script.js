acc2c0da2b4b0d60cc5cf1935b6cf9689bff80e2
Fabyan Yastika Permana <fabyanxrpl@gmail.com>
2023-12-24 12:27:24 +0700

    add slider

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-const swiper = new Swiper(".mySwiper", {
+const rem = 16; // 1rem is 16px
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 1,
+  spaceBetween: rem * 2,
+  mousewheel: false,
+  // allowTouchMove: false,
+
+  // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
   },
 });
+
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: false,
+//   slidesPerView: '1',
+//   spaceBetween: 32,
+
+//   // Navigation arrows
+//   navigation: {
+//     prevEl: '.swiper-prev',
+//     nextEl: '.swiper-next',
+//   },
+
+//   freeMode: false, // при перетаскивании превью ведет себя как при скролле
+//   mousewheel: false,
+//   allowTouchMove: false,
+
+// });
